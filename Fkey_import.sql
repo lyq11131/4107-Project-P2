c9f8af3dc909b851525a1986b363e648e0b5a8c3
Yongquan Long <34349524+lyq11131@users.noreply.github.com>
2024-03-27 21:35:57 -0400

    Update Fkey_import.sql

--- Fkey_import.sql
+++ Fkey_import.sql
@@ -1,22 +1,22 @@
--- 更新事实表中的 DemographicsID
+-- Update the DemographicsID in the fact table.
 UPDATE PatientHealthFacts
 SET DemographicsID = Demographics.DemographicsID
 FROM Demographics
 WHERE PatientHealthFacts.PatientID = Demographics.DemographicsID;
 
--- 更新事实表中的 NutrientIntakeID
+-- Update the NutrientIntakeID in the fact table.
 UPDATE PatientHealthFacts
 SET NutrientIntakeID = NutrientIntake.NutrientIntakeID
 FROM NutrientIntake
 WHERE PatientHealthFacts.PatientID = NutrientIntake.NutrientIntakeID;
 
--- 更新事实表中的 livingHabitsID
+-- Update the livingHabitsID in the fact table. 
 UPDATE PatientHealthFacts
 SET livingHabitsID = livingHabits.livingHabitsID
 FROM livingHabits
 WHERE PatientHealthFacts.PatientID = livingHabits.livingHabitsID;
 
--- 更新事实表中的 MedicalConditionID
+-- Update the MedicalConditionID in the fact table. 
 UPDATE PatientHealthFacts
 SET MedicalConditionID = MedicalCondition.MedicalConditionID
 FROM MedicalCondition
